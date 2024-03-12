ccd42d034c2d0d8f7c8744e46c6e1a791c96d4b0
bakradzetamar <156605663+bakradzetamar@users.noreply.github.com>
2024-03-12 22:41:06 +0400

    3

--- script.js
+++ script.js
@@ -14,3 +14,60 @@ divContainer.forEach((el) => {
     this.classList.toggle("active");
   });
 });
+
+
+
+
+
+
+// const waitFnc = function (seconds) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+// const imageContainer = document.querySelector(".images");
+// let currentImage;
+
+// const createImgElement = function (imagePath) {
+//   return new Promise((resolve, reject) => {
+//     const imgEl = document.createElement("img");
+//     imgEl.src = imagePath;
+
+//     imgEl.addEventListener("load", function () {
+//       imageContainer.appendChild(imgEl);
+//       resolve(imgEl);
+//     });
+
+//     imgEl.addEventListener("error", function () {
+//       reject(new Error("Image Not Found"));
+//     });
+//   });
+// };
+
+// createImgElement("im/DF-301D-4.jpg")
+//   .then((image) => {
+//     currentImage = image;
+//     console.log("Loaded Image");
+//     return waitFnc(5);
+//   })
+//   .then(() => {
+//     currentImage.style.display = "none";
+//     return createImgElement("im/ezgif.com-gif-maker-29.jpg");
+//   })
+//   .then((image2) => {
+//     currentImage = image2;
+//     console.log("Image 2 is loaded");
+//     return wait(7);
+//   })
+//   .then(() => {
+//     currentImage.style.display = "none";
+//     return createImgElement("im/DF-301D-4.jpg");
+//   })
+
+//     .then((image) => {
+//       currentImage = image;
+//       console.log("Loaded Image");
+//       return waitFnc(5);
+//     })
+  
+//   .catch((error) => console.log(error));
